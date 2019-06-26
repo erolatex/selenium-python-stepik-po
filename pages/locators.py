0116cb1208eaf82c0b788be06c89488160f0540d
erolatex <rf@sudouser.ru>
2019-06-26 16:41:07 +0300

    Add some changes

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,12 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators(object):
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators(object):
+    LOGIN_EMAIL_FIELD = (By.CSS_SELECTOR, "#id_login-username")
+    LOGIN_PASSWORD_FIELD = (By.CSS_SELECTOR, "#id_login-password")
+    REGISTRATION_EMAIL_FIELD = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTRATION_PASSWORD_FIELD = (By.CSS_SELECTOR, "#id_registration-password1")
+    REPEAT_PASSWORD_FIELD = (By.CSS_SELECTOR, "#id_registration-password2")
+
+
