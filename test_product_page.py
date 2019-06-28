a7932399072e8c59bfbd7bfbe1134d4e9c3d17b4
erolatex <rf@sudouser.ru>
2019-06-28 17:04:18 +0300

    Lesson 4.3 step 11

--- test_product_page.py
+++ test_product_page.py
@@ -7,6 +7,12 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
 #urls = [f"{product_base_link}/?promo=offer{no}" for no in range(10)]
 
 #@pytest.mark.parametrize('link', urls)
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
 def guest_can_add_product_to_cart(browser, link):
     page = ProductPage(browser, link)
     page.open()
