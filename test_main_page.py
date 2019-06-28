f046d27d16da682917440d33a8d515f063affe1a
erolatex <rf@sudouser.ru>
2019-06-28 11:54:11 +0300

    Add 4.2 PageObject

--- test_main_page.py
+++ test_main_page.py
@@ -1,18 +1,19 @@
 from pages.main_page import MainPage
 
 
-MAIN_PAGE = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=midsummer"
+#MAIN_PAGE = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=midsummer"
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = MAIN_PAGE
+    link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
     page.go_to_login_page()  # выполняем метод страницы - переходим на страницу логина
 
 def test_guest_should_see_login_link(browser):
-    link = MAIN_PAGE
+    link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
     page.open()
-    page.should_be_login_link()
+    login_page = page.go_to_login_page()
+    login_page.should_be_login_page()
 
