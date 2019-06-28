1dbc6210df11190d49dc76b30e11a41ff39f741a
erolatex <rf@sudouser.ru>
2019-06-28 16:02:10 +0300

    Add 4.3 step 6

--- pages/product_page.py
+++ pages/product_page.py
@@ -33,3 +33,13 @@ class ProductPage(BasePage):
 
 
         assert product_price in message_basket_total, "Message should have total amount"
+
+
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.MESSAGE_ABOUT_ADDING), \
+           "Success message is presented, but should not be"
+
+    def should_be_disappeared_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.MESSAGE_ABOUT_ADDING), \
+           "Success message is presented, but should not be"
--- test_product_page.py
+++ test_product_page.py
@@ -2,11 +2,12 @@ from pages.product_page import ProductPage
 import time
 import pytest
 
-product_base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
-urls = [f"{product_base_link}/?promo=offer{no}" for no in range(10)]
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+#product_base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+#urls = [f"{product_base_link}/?promo=offer{no}" for no in range(10)]
 
-@pytest.mark.parametrize('link', urls)
-def test_guest_can_add_product_to_cart(browser, link):
+#@pytest.mark.parametrize('link', urls)
+def guest_can_add_product_to_cart(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.add_to_backet()
@@ -14,3 +15,21 @@ def test_guest_can_add_product_to_cart(browser, link):
     page.should_be_message_about_adding()
     page.should_be_message_basket_total()
 #    time.sleep(10)
+
+def test_guest_cant_see_success_message_after_adding_product_to_cart(browser):
+    page = ProductPage(browser,link)
+    page.open()
+    page.add_to_backet()
+    page.should_not_be_success_message()
+
+def test_guest_cant_see_success_message(browser):
+    page = ProductPage(browser,link)
+    page.open()
+    page.should_not_be_success_message()
+
+def test_message_dissapeared_after_adding_product_to_cart(browser):
+    page = ProductPage(browser,link)
+    page.open()
+    page.add_to_backet()
+    page.should_be_disappeared_success_message()
+
