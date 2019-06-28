5b0cd198a4c103db6cc3a44ec5d8babf17498545
erolatex <rf@sudouser.ru>
2019-06-28 11:00:31 +0300

    Add LoginPage test

--- pages/locators.py
+++ pages/locators.py
@@ -5,10 +5,9 @@ class MainPageLocators(object):
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 class LoginPageLocators(object):
-    LOGIN_EMAIL_FIELD = (By.CSS_SELECTOR, "#id_login-username")
-    LOGIN_PASSWORD_FIELD = (By.CSS_SELECTOR, "#id_login-password")
-    REGISTRATION_EMAIL_FIELD = (By.CSS_SELECTOR, "#id_registration-email")
-    REGISTRATION_PASSWORD_FIELD = (By.CSS_SELECTOR, "#id_registration-password1")
-    REPEAT_PASSWORD_FIELD = (By.CSS_SELECTOR, "#id_registration-password2")
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    
+    
 
 
